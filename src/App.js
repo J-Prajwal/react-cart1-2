20ff700e73bdba230e04bb8d198aa47ca6055d2f
BackbenchCoder-py <80917802+BackbenchCoder-py@users.noreply.github.com>
2022-06-05 20:32:28 +0530

    React cart assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Mens from './Pages/Mens';
+import Womens from './Pages/Womens';
+import Kids from './Pages/Kids';
+import Login from './Pages/Login';
+import Details from './Pages/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/Mens' element={<Mens/>}></Route>
+        <Route path='/Womens' element={<Womens/>}></Route>
+        <Route path='/Kids' element={<Kids/>}></Route>
+        <Route path='/Login' element={<Login/>}></Route>
+        <Route path='/Details' element={<Details />} ></Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ProdContextProvider } from "./Contexts/ProdContext";
+import { AuthContext } from "./Contexts/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <AuthContext> */}
+        <ProdContextProvider>
+          <App />
+        </ProdContextProvider>
+      {/* </AuthContext> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
